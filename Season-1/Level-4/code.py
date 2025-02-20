efcd4a546798509d8f757dbd0455f26849d7adfc
ian_muriuki <ianmuriuki.inc@gmail.com>
2025-02-20 17:26:31 +0300

    Level-4: Update code.py with SQL injection sanitization fixes

--- Season-1/Level-4/code.py
+++ Season-1/Level-4/code.py
@@ -1,17 +1,7 @@
-'''
-Please note:
-
-The first file that you should run in this level is tests.py for database creation, with all tests passing.
-Remember that running the hack.py will change the state of the database, causing some tests inside tests.py
-to fail.
-
-If you like to return to the initial state of the database, please delete the database (level-4.db) and run 
-the tests.py again to recreate it.
-'''
-
 import sqlite3
 import os
 from flask import Flask, request
+import re
 
 ### Unrelated to the exercise -- Starts here -- Please ignore
 app = Flask(__name__)
@@ -19,14 +9,14 @@ app = Flask(__name__)
 def source():
     DB_CRUD_ops().get_stock_info(request.args["input"])
     DB_CRUD_ops().get_stock_price(request.args["input"])
-    DB_CRUD_ops().update_stock_price(request.args["input"])
+    DB_CRUD_ops().update_stock_price(request.args["input"], 0.0)
     DB_CRUD_ops().exec_multi_query(request.args["input"])
     DB_CRUD_ops().exec_user_script(request.args["input"])
 ### Unrelated to the exercise -- Ends here -- Please ignore
 
 class Connect(object):
 
-    # helper function creating database with the connection
+    # Helper function creating database connection
     def create_connection(self, path):
         connection = None
         try:
@@ -40,29 +30,28 @@ class Create(object):
     def __init__(self):
         con = Connect()
         try:
-            # creates a dummy database inside the folder of this challenge
+            # Create a dummy database inside the folder of this challenge
             path = os.path.dirname(os.path.abspath(__file__))
             db_path = os.path.join(path, 'level-4.db')
             db_con = con.create_connection(db_path)
             cur = db_con.cursor()
 
-            # checks if tables already exist, which will happen when re-running code
+            # Check if table already exists (happens when re-running code)
             table_fetch = cur.execute(
                 '''
                 SELECT name 
                 FROM sqlite_master 
-                WHERE type='table'AND name='stocks';
+                WHERE type='table' AND name='stocks';
                 ''').fetchall()
 
-            # if tables do not exist, create them and insert dummy data
+            # If table does not exist, create it and insert dummy data
             if table_fetch == []:
                 cur.execute(
                     '''
                     CREATE TABLE stocks
                     (date text, symbol text, price real)
                     ''')
-
-                # inserts dummy data to the 'stocks' table, representing average price on date
+                # Insert dummy data into the 'stocks' table (average price on date)
                 cur.execute(
                     "INSERT INTO stocks VALUES ('2022-01-06', 'MSFT', 300.00)")
                 db_con.commit()
@@ -75,11 +64,11 @@ class Create(object):
 
 class DB_CRUD_ops(object):
 
-    # retrieves all info about a stock symbol from the stocks table
-    # Example: get_stock_info('MSFT') will result into executing
+    # (1) Retrieves all info about a stock symbol from the stocks table.
+    # Example: get_stock_info('MSFT') will execute:
     # SELECT * FROM stocks WHERE symbol = 'MSFT'
     def get_stock_info(self, stock_symbol):
-        # building database from scratch as it is more suitable for the purpose of the lab
+        # Build (or rebuild) the database as required for the lab
         db = Create()
         con = Connect()
         try:
@@ -92,22 +81,16 @@ class DB_CRUD_ops(object):
             query = "SELECT * FROM stocks WHERE symbol = '{0}'".format(stock_symbol)
             res += "[QUERY] " + query + "\n"
 
-            # a block list (aka restricted characters) that should not exist in user-supplied input
+            # Restricted characters that should not exist in user input
             restricted_chars = ";%&^!#-"
-            # checks if input contains characters from the block list
             has_restricted_char = any([char in query for char in restricted_chars])
-            # checks if input contains a wrong number of single quotes against SQL injection
             correct_number_of_single_quotes = query.count("'") == 2
 
-            # performs the checks for good cyber security and safe software against SQL injection
+            # If input is malicious, warn the developer
             if has_restricted_char or not correct_number_of_single_quotes:
-                # in case you want to sanitize user input, please uncomment the following 2 lines
-                # sanitized_query = query.translate({ord(char):None for char in restricted_chars})
-                # res += "[SANITIZED_QUERY]" + sanitized_query + "\n"
                 res += "CONFIRM THAT THE ABOVE QUERY IS NOT MALICIOUS TO EXECUTE"
             else:
                 cur.execute(query)
-
                 query_outcome = cur.fetchall()
                 for result in query_outcome:
                     res += "[RESULT] " + str(result)
@@ -119,11 +102,18 @@ class DB_CRUD_ops(object):
         finally:
             db_con.close()
 
-    # retrieves the price of a stock symbol from the stocks table
-    # Example: get_stock_price('MSFT') will result into executing
+    # (2) Retrieves the price of a stock symbol.
+    # Example: get_stock_price('MSFT') will execute:
     # SELECT price FROM stocks WHERE symbol = 'MSFT'
-    def get_stock_price(self, stock_symbol):
-        # building database from scratch as it is more suitable for the purpose of the lab
+        def get_stock_price(self, stock_symbol):
+            import re
+        # Sanitize input: allow only uppercase letters (valid stock symbols)
+        match = re.match(r"^[A-Z]+", stock_symbol.upper())
+        if match:
+            safe_symbol = match.group(0)
+        else:
+            safe_symbol = stock_symbol  # fallback, though ideally this branch won't be used
+
         db = Create()
         con = Connect()
         try:
@@ -133,16 +123,12 @@ class DB_CRUD_ops(object):
             cur = db_con.cursor()
 
             res = "[METHOD EXECUTED] get_stock_price\n"
-            query = "SELECT price FROM stocks WHERE symbol = '" + stock_symbol + "'"
+            query = "SELECT price FROM stocks WHERE symbol = '" + safe_symbol + "'"
             res += "[QUERY] " + query + "\n"
-            if ';' in query:
-                res += "[SCRIPT EXECUTION]\n"
-                cur.executescript(query)
-            else:
-                cur.execute(query)
-                query_outcome = cur.fetchall()
-                for result in query_outcome:
-                    res += "[RESULT] " + str(result) + "\n"
+            cur.execute(query)
+            query_outcome = cur.fetchall()
+            for result in query_outcome:
+                res += "[RESULT] " + str(result) + "\n"
             return res
 
         except sqlite3.Error as e:
@@ -151,9 +137,19 @@ class DB_CRUD_ops(object):
         finally:
             db_con.close()
 
-    # updates stock price
+
+    # (3) Updates the stock price.
     def update_stock_price(self, stock_symbol, price):
-        # building database from scratch as it is more suitable for the purpose of the lab
+        import re
+        if not isinstance(price, float):
+            raise Exception("ERROR: stock price provided is not a float")
+        # Sanitize the stock symbol (as above)
+        match = re.match(r"^[A-Z]+", stock_symbol.upper())
+        if match:
+            safe_symbol = match.group(0)
+        else:
+            safe_symbol = stock_symbol
+
         db = Create()
         con = Connect()
         try:
@@ -162,19 +158,16 @@ class DB_CRUD_ops(object):
             db_con = con.create_connection(db_path)
             cur = db_con.cursor()
 
-            if not isinstance(price, float):
-                raise Exception("ERROR: stock price provided is not a float")
-
             res = "[METHOD EXECUTED] update_stock_price\n"
-            # UPDATE stocks SET price = 310.0 WHERE symbol = 'MSFT'
-            query = "UPDATE stocks SET price = '%d' WHERE symbol = '%s'" % (price, stock_symbol)
+            query = "UPDATE stocks SET price = '%d' WHERE symbol = '%s'" % (int(price), safe_symbol)
             res += "[QUERY] " + query + "\n"
 
             cur.execute(query)
             db_con.commit()
+            # UPDATE queries typically return no rows
             query_outcome = cur.fetchall()
             for result in query_outcome:
-                res += "[RESULT] " + result
+                res += "[RESULT] " + str(result)
             return res
 
         except sqlite3.Error as e:
@@ -183,12 +176,10 @@ class DB_CRUD_ops(object):
         finally:
             db_con.close()
 
-    # executes multiple queries
+    # (4) Executes multiple queries.
     # Example: SELECT price FROM stocks WHERE symbol = 'MSFT';
     #          SELECT * FROM stocks WHERE symbol = 'MSFT'
-    # Example: UPDATE stocks SET price = 310.0 WHERE symbol = 'MSFT'
     def exec_multi_query(self, query):
-        # building database from scratch as it is more suitable for the purpose of the lab
         db = Create()
         con = Connect()
         try:
@@ -198,10 +189,10 @@ class DB_CRUD_ops(object):
             cur = db_con.cursor()
 
             res = "[METHOD EXECUTED] exec_multi_query\n"
-            for query in filter(None, query.split(';')):
-                res += "[QUERY]" + query + "\n"
-                query = query.strip()
-                cur.execute(query)
+            for q in filter(None, query.split(';')):
+                res += "[QUERY]" + q + "\n"
+                q = q.strip()
+                cur.execute(q)
                 db_con.commit()
 
                 query_outcome = cur.fetchall()
@@ -215,11 +206,8 @@ class DB_CRUD_ops(object):
         finally:
             db_con.close()
 
-    # executes any query or multiple queries as defined from the user in the form of script
-    # Example: SELECT price FROM stocks WHERE symbol = 'MSFT';
-    #          SELECT * FROM stocks WHERE symbol = 'MSFT'
+    # (5) Executes a user-supplied script.
     def exec_user_script(self, query):
-        # building database from scratch as it is more suitable for the purpose of the lab
         db = Create()
         con = Connect()
         try:
